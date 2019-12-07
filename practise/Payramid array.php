237ec1658a647e68c0ed8d87aead0bced59b6a21
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2019-12-07 16:04:19 +0500

    Create Payramid array.php

---
+++ practise/Payramid array.php
@@ -0,0 +1,15 @@
+<?php
+function mine(){
+    $a = 5;
+    
+for($i = 0; $i < $a; $i++){
+    for($x=0; $x <= $i; $x++)
+    {
+        
+        echo "*";
+    }
+    echo "<br/>";
+}
+    }
+mine();
+?>
