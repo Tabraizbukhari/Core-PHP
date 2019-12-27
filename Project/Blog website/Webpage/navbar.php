3c73f10dcdb39a8915fa91010e5bb08f7fceb749
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2019-12-27 18:16:08 +0500

    Update navbar.php

--- Project/Blog website/Webpage/navbar.php
+++ Project/Blog website/Webpage/navbar.php
@@ -1,3 +1,38 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myweb", $username, $password);
+   
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+   $sql =("SELECT * FROM navbar   ");
+   $stmt = $conn->prepare($sql);
+   $stmt->execute();
+
+   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+   $navbar = $stmt->fetchAll();
+
+   $sqls =("SELECT * FROM logo ORDER BY id DESC   ");
+   $stmt = $conn->prepare($sqls);
+   $stmt->execute();
+
+   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+   $logo = $stmt->fetch();
+
+
+  }catch(PDOException $e)
+  {
+  echo "Connection failed: " . $e->getMessage();
+  }
+
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -26,17 +61,16 @@
   <body>
 	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.php">Stories<span>.</span></a>
+	      <a class="navbar-brand" href="index.php"><?php echo $logo['logo']; ?><span>.</span></a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
-	          <li class="nav-item active"><a href="artical.php?page=1" class="nav-link">Blogs</a></li>
-	          <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+      <?php
+      foreach($navbar as $nav){ echo'     <li class="nav-item"><a href="'.$nav['link'].'" class="nav-link">'.$nav['pagename'].'</a></li> ';} ?>
+	        
 	        </ul>
 	      </div>
 	    </div>
