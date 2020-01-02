ead84b2e87ad098b45cdd65ab232162342bc678e
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-02 18:21:16 +0500

    Array Count function

---
+++ practise/Array programs/count.php
@@ -0,0 +1,12 @@
+<?php
+function counting($arr){   
+   $count= 0;
+
+    foreach ($arr as $ar) {
+        $count++;
+    }
+       return($count); 
+   }
+//    $arr = array(1,2,4,5,6);
+//    echo counting($arr);
+   ?>
