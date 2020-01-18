98503c6b50a5999c3016ce3218b397153528a1e3
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-18 16:27:36 +0500

    Create logincss.php

---
+++ Project/CMS/admin/include/logincss.php
@@ -0,0 +1,43 @@
+<?php include "database.php" ?>
+<?php include "function.php" ?>
+
+<?php ob_start(); ?>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  rel="stylesheet">
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"  type="text/javascript"></script>
+<style>
+.center_div{
+    padding-top:350px;
+    margin: 0 auto;
+    width:350px;
+    
+}
+.registration_center_div{
+    
+    width:800px;
+    border: 1px solid;
+  margin-left: 640px;
+  margin-top:155px;
+  box-shadow: 5px 10px 8px #888888;
+    
+}
+
+.bot{
+    margin:0 auto;
+    width:100px;
+}
+.log{
+    margin:0 auto;
+    width:180px;
+    padding-bottom:20px;
+    margin-right:70px;
+    font-family:bold ;
+}
+.creat{
+    margin: 0 auto;
+    width: 160px;
+}
+
+
+</style>
