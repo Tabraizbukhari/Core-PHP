d49e1e5f9bef8c86ff724f3345bc54f5f73ce730
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-18 16:21:12 +0500

    Create contact.php

---
+++ Project/CMS/admin/contact.php
@@ -0,0 +1,27 @@
+<?php 
+
+if(isset($_GET['source'])){
+
+    $source =$_GET['source'];
+
+}else{
+    $source ="";
+}
+
+switch ($source) {
+   
+        //  case 'comment_id':
+        //     include 'include/comment.php';
+        // break;
+        // case 'category':
+        //     include 'include/category.php';
+        // break;
+        // case 'editpost':
+        //     include 'include/editpost.php';
+        // break;
+        
+     default:
+       include 'include/viewallcontact.php';
+        break;
+}
+?>
