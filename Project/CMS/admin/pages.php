9e36ff0b4df6337f96255607dd0cc8244c97b348
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-18 16:21:50 +0500

    Create pages.php

---
+++ Project/CMS/admin/pages.php
@@ -0,0 +1,27 @@
+<?php 
+
+if(isset($_GET['source'])){
+
+    $source =$_GET['source'];
+
+}else{
+    $source ="";
+}
+
+switch ($source) {
+   
+         case 'addabout':
+            include 'include/addabout.php';
+        break;
+        case 'editabout':
+            include 'include/editabout.php';
+        break;
+        // case 'editpost':
+        //     include 'include/editpost.php';
+        // break;
+        
+     default:
+       include 'include/viewallabout.php';
+        break;
+}
+?>
