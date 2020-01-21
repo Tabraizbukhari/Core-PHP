677ff77728aac58ae1a32b29d7df1077ae5462d0
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-21 16:43:56 +0500

    english language

---
+++ Project/CMS/admin/include/laqnguage/en.php
@@ -0,0 +1,24 @@
+<?php
+
+const _register = "Register";
+const _first = "Enter your username";
+const _last  = "Enter your lastname";
+const _email = "Enter your email address";
+const _password = "Enter your password";
+const _city = "Enter your city";
+const _address = "Enter your address";
+
+
+const _lmale = "male";
+const _lfemale = "female";
+const _lfirst = "First name";
+const _llast = "last name";
+const _lemail = "email address";
+const _lbirthday = "Birthday";
+const _lcity = "city";
+const _laddress = "address";
+const _lpass = "password";
+const _lcompass = "confirm password";
+
+
+?>
