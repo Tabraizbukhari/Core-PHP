6f5f629690271fbdeb384688215b1d1dd89134a8
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-22 10:51:43 +0500

    Create es.php

---
+++ Project/CMS/admin/include/language/es.php
@@ -0,0 +1,23 @@
+<?php
+
+const _register = "Registrarse";
+const _first = "ponga su primer nombre";
+const _last  = "ingresa tu apellido";
+const _email = "Introduce tu correo electrónico";
+const _password = "Ingresa tu contraseña";
+const _city = "ingresa tu ciudad";
+const _address = "ingrese su dirección";
+
+const _lmale = "masculino";
+const _lfemale = "femenino";
+
+const _lfirst = "nombre de pila";
+const _llast = "apellido";
+const _lemail = "el e-mail";
+const _lbirthday = "cumpleaños";
+const _lcity = "ciudad";
+const _laddress = "habla a";
+const _lpass = "contraseña";
+const _lcompass = "Confirmar contraseña";
+
+?>
