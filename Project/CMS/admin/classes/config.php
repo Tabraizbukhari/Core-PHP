4780973df6dc5e1cbd203b4446b49289ef2a9e66
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-22 18:44:42 +0500

    Create config.php

---
+++ Project/CMS/admin/classes/config.php
@@ -0,0 +1,11 @@
+<?php
+    class config{
+
+        const SMTP_HOST ='smtp.mailtrap.io';
+        const SMTP_PORT ='2525';
+        const SMTP_USER ='b9d4529642a04c';
+        const SMTP_PASSWORD ='bb109f1aea84d3';
+         
+    }
+    
+?>
