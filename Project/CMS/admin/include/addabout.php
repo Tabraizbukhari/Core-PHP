82a2b02ff8aea4cd2e2695a41c1ff8984a67b9af
Tabraizbukhari <58254222+Tabraizbukhari@users.noreply.github.com>
2020-01-23 19:04:10 +0500

    Update addabout.php

--- Project/CMS/admin/include/addabout.php
+++ Project/CMS/admin/include/addabout.php
@@ -1,61 +1,104 @@
-<?php include "include/header.php" ?>
-<?php include "include/navbar.php" ?>
-<?php addabout(); ?>
+
 <?php $name = $_SERVER['PHP_SELF']; ?>
+
+
+
 <div class="content-wrapper">
+
     <div class="container-fluid">
       <!-- Breadcrumbs-->
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
-          <a href="index.html">Dashboard</a>
+          <a href="#">Dashboard</a>
         </li>
-        <li class="breadcrumb-item active"><?php if(isset($name)){ echo basename($name);}?></li>
+        <li class="breadcrumb-item active"><?php if(isset($name)){ echo $name; } ?></li>
       </ol>
-      
-      <form method="post" enctype="multipart/form-data">
-    <div class="row d-flex justify-content-center">    
-      <div class="col-9" >
-  
-  <div class="form-group">
-    <input type="text" name="head" class="form-control" id="exampleFormControlInput1" placeholder="Enter about Heading ..!">
-  </div>
- 
-  <div class="form-group">
-    <label for="exampleFormControlFile1">About IMAGE</label>
-    <input type="text" name="title" class="form-control" id="exampleFormControlInput1" placeholder="Image Title..!">
-    <input type="file" name="abt_img" class="form-control-file" id="exampleFormControlFile1">
-  </div>
- 
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">About content</label>
-    <textarea name="content" class="form-control" id="body" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <div class="form-group">
-    <input type="text" name="tags" class="form-control" id="exampleFormControlFile1" placeholder="Blog Tags">
-  </div>
-  <div class="form-group">
-  <!-- <div class="form-group">
-        <label for="exampleFormControlSelect1">What you want to do?</label>
-        <select required name="status" class="form-control" id="exampleFormControlSelect2">
-        <option value="" >Select any one</option>
-        <option value="published" >published</option>
-        <option value="draft" >draft</option>
-        </select>
-        </div>
-    
-  </div> -->
-  <div class="form-group">
-    <button name="addabout" class="btn btn-danger float-right btn-lg" style="margin-top:20px;" name="add_post" >Submit</button>  
-  </div>
-  
-        </div> 
-        
-    </div>
-     
-        
-  </div>
-</form>
 
+<div class="col-md-8">
+<form method="post" enctype="multipart/form-data">
+  </div>
+<div class="form-row">
+    <div class="form-group col-md-6">
+      <input type="text" class="form-control" required name="fname" placeholder="First name">
+    </div>
+    <div class="form-group col-md-6">
+    <input type="text" class="form-control" required name="lname"  placeholder="Last name">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+    <label for="inputAddress">Age:</label>
+      <input type="date" class="form-control" required name="age" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+    <label for="inputAddress">Email:</label>
+      <input type="email" class="form-control" required name="email" placeholder="useremail">
+    </div>
+  </div>
+ 
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" required name="city" placeholder="City name">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputZip">Address</label>
+      <input type="text" class="form-control" required name="address" placeholder="User address">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputState">Role:</label>
+      <select id="inputState" name="role" class="form-control">
+        <option selected>Choose here...</option>
+        <option>commenter</option>
+        <option>blogpost</option>
+      </select>
     </div>
    
-<?php include "include/footer.php" ?>
+  </div>
+  
+  <div class="form-row">
+  
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-4">
+    
+          <input type="password" required name="pass" class="form-control" placeholder="password">
+    </div>
+    <div class="form-group col-md-4">
+    
+    <?php  echo '<span style="color:red;"> '; adduser(); echo'</span>';?>
+      <input type="password" required name="compass" class="form-control" placeholder="Conform password">
+    </div>
+    <div class="col-md-1">
+  <label for="inputState">Gender: &nbsp;</label>
+  </div>
+  <div class="col-md-1">
+  <input type="radio" name="gender"> 
+    <label>Male</label>
+    </div>
+    <div class="col-md-2">
+    <input type="radio" name="gender"> 
+    <label>Female</label>
+    </div>
+    
+    </div>
+    <div class="form-row">
+    <div class="form-group col-md-3">
+          <input type="file"  name="userimg" class="form-control" placeholder="password">
+    </div>
+    
+    <div class="form-group col-md-8">
+    <button  name="adduser" class="btn btn-danger float-right ">Create new account</button>
+    </div>
+    </div>
+   
+</form>
+
+</div>
+
+</div>
+  </div>
+
+
+  <?php include "include/footer.php" ?>
+
